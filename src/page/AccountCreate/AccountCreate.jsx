d3f3145ce4b389dfe526613f6ca31ac1c6688163
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-08-01 00:24:13 +0600

    account create page added

--- src/page/AccountCreate/AccountCreate.jsx
+++ src/page/AccountCreate/AccountCreate.jsx
@@ -1,7 +1,110 @@
 function AccountCreate() {
     return (
-        <div>
-            <h1>AccountCreate</h1>
+        <div className="p-4">
+            {/* Title */}
+            <h1 className="text-3xl md:text-5xl font-medium ">
+                কিভাবে একাউন্ট খুলবেন?
+            </h1>
+
+            <button className="border border-gray-500 hover:bg-[#c00] hover:text-white px-2 text-sm transition-all ease-in hover:border-transparent mt-8">
+                Velki FAQ
+            </button>
+
+            {/* first content */}
+            <div className="border-l-4 px-4 my-8">
+                <h1 className="text-lg md:text-xl font-bold mb-2">
+                    একাউন্ট আপনি নিজে নিজে খুলতে পারবেন না। তাই একাউন্ট খোলার
+                    আগে মনোযোগ দিয়ে পড়ুনঃ
+                </h1>
+
+                <p className="text-gray-800 mb-3">
+                    ভেল্কি তে একাউন্ট করতে হলে আপনার এজেন্ট এর মাধ্যমে একাউন্ট
+                    খুলতে হবে। এজেন্ট এর মাধ্যমেই টাকা ডিপোজিট এবং উইথড্র করতে
+                    হবে। আপনি যে এজেন্ট এর কাছ থেকে একাউন্ট খুলবেন তার সাথেই সব
+                    সময় লেনদেন করতে হবে। ঠিক কোন এজেন্ট কে টাকা দিবেন এবং কিভাবে
+                    তার সাথে লেনদেন করবেন তার বুঝতে হলে আপনার নিম্বের তথ্য গুলো
+                    পড়া জরুরী।
+                </p>
+
+                <p className="font-bold">
+                    আমাদের এজেন্ট লিষ্ট দেখতে নিচের লিঙ্ক এ ক্লিক করুনঃ
+                </p>
+
+                <a
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-[#C00] hover:underline transition-all ease-in"
+                >
+                    https://allagentlist.com/ma.php
+                </a>
+            </div>
+
+            {/* Second content */}
+            <p className="my-2 text-gray-800">
+                এই এজেন্ট লিষ্ট এর বাইরে কারো সাথে লেনদেন করলে আপনাকে নিজ
+                দ্বায়িত্বে লেনদেন করতে হবে। লিষ্ট এর বাইরে কোন এজেন্ট এর
+                দায়ভার ভেল্কি নিবে না এবং লিষ্ট এর এজেন্ট দের সব দায়ভার ভেল্কি
+                কোম্পানীর। এজেন্ট লিষ্ট এর এজেন্ট দের সাথে ইউজার দের শুধু মাত্র
+                হোয়াটসাপ এর মাধ্যমে যোগাযোগ করতে হবে। হোয়াটসাপ ছাড়া অন্য কোন
+                মাধ্যমে যোগাযোগ করলে বা লেনদেন করলে তা গ্রহনযোগ্য হবে না।
+                হোয়াটসাপ এ যোগাযোগ করতে হলে এজেন্ট লিস্টে হোয়াটসাপ আইকন উপরে
+                ক্লিক করুন অবথা ফোন নাম্বার টি মোবাইলে সেভ করে তাকে হোয়াটসাপ এ
+                মসেজ পাঠাতে পারবেন। হোয়াটসাপ এপ টি আপনার মোবাইলে আগে থেকেই
+                থাকতে হবে। না থাকলে গুগুল প্লে থেকে ইন্সটল করে নিন।
+            </p>
+
+            {/* third content */}
+            <div className="space-y-1">
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    অন্যান্য সাহায্যের জন্যঃ তথ্যাদি এবং সাহায্যের জন্য
+                    হোয়াটসাপ নাম্বারঃ{" "}
+                    <a
+                        href="/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[#C00] hover:underline transition-all ease-in ml-1"
+                    >
+                        +15714028916
+                    </a>{" "}
+                    (আকাশ মালিক)
+                </p>
+
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    ফেসবুক গ্রুপের লিঙ্কঃ
+                    <a
+                        href="/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[#C00] hover:underline transition-all ease-in ml-1"
+                    >
+                        https://www.facebook.com/groups/velki.live
+                    </a>
+                </p>
+
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    এই ফেসবুক গ্রুপ এবং একটি ব্যাকআপ গ্রুপ ছাড়া আমাদের আর কোন
+                    ফেসবুক গ্রুপ নেই।
+                </p>
+
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    এছাড়া আপনারা আমাদের কাস্টমার সার্ভিস এর সাথে সরাসরি যোগাযোগ
+                    করতে পারেন।
+                </p>
+
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    তাদের নাম্বার গুলো জানতে নিচের লিঙ্কে ক্লিক করুনঃ
+                    <a href="/" target="_blank" rel="noopener noreferrer" className="text-[#C00] hover:underline transition-all ease-in ml-1">
+                        https://allagentlist.com/cs.php
+                    </a>
+                </p>
+
+                <p className="text-gray-800 text-xs md:text-sm font-semibold">
+                    একাউন্ট খোলার আগে নিচের লিঙ্কে ক্লিক করে শর্ত সমুহ জেনে নিন।
+                    এই শর্ত সমুহ জানা অত্যন্ত জরুরীঃ নিয়মগুলো জানার জন্য এই
+                    লিঙ্ক এ ক্লিক করুন।{" "}
+                </p>
+            </div>
         </div>
     );
 }
