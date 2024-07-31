e83b4c97753cdfd918f9c4644ec692f813b5525f
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-07-31 11:25:42 +0600

    Down navbars hover effect added

--- src/Components/Shared/Navbar/DownNavbar.jsx
+++ src/Components/Shared/Navbar/DownNavbar.jsx
@@ -1,11 +1,61 @@
 import { NavLink } from "react-router-dom";
+import { FaCaretRight } from "react-icons/fa6";
+import Links from "./Links";
+import { useState } from "react";
 
 function DownNavbar() {
+    const [isCommon, setIsCommon] = useState(false);
+    const [isCommon2, setIsCommon2] = useState(false);
+    const links = [
+        {
+            path: "/proxyLink",
+            title: "ভেল্কি প্রক্সী লিঙ্ক",
+        },
+        {
+            path: "/accountCreate",
+            title: "ভেল্কিতে একাউন্ট খুলবেন?",
+        },
+        {
+            path: "/agentNewNumber",
+            title: "এজেন্ট এর নতুন নাম্বার",
+        },
+        {
+            path: "/phoneNumberSearch",
+            title: "ফোন নাম্বার সার্চ করুন",
+        },
+    ];
+    const agentLinks = [
+        {
+            path: "/agentVerify",
+            title: "এজেন্ট কে ভেরিফাই করুন",
+        },
+        {
+            path: "/masterAgent",
+            title: "মাষ্টার এজেন্ট লিষ্ট",
+        },
+        {
+            path: "/superAgent",
+            title: "সুপার এজেন্ট লিষ্ট",
+        },
+        {
+            path: "/subAdmin",
+            title: "সাব এডমিন লিষ্ট",
+        },
+        {
+            path: "/siteAdmin",
+            title: "সাইট এডমিন লিষ্ট",
+        },
+        {
+            path: "/CustomerService",
+            title: "কাস্টমার সার্ভিস লিষ্ট",
+        },
+    ];
+
     return (
         <div>
-            <hr className="bg-[#c00] h-[2px] m-[5px] p-0" />
+            <hr className="bg-[#c00] h-[2px] mt-[5px] p-0 z-20 relative" />
 
-            <div className="w-[70%] mx-auto">
+            <div className="w-[70%] mx-auto flex items-center">
                 <NavLink
                     to={"/autoDeposit"}
                     className={({ isActive }) =>
@@ -28,16 +78,48 @@ function DownNavbar() {
                     হোম পেইজ
                 </NavLink>
 
-                <button className="bg-transparent text-black transition-all ease-in font-medium text-lg border-x border-[#E5E5E5] py-2 px-4 hover:bg-black hover:text-white">
-                    সাধারন প্রশ্ন উত্তর
+                <button
+                    onClick={() => setIsCommon(!isCommon)}
+                    className="bg-transparent text-black transition-all ease-in font-medium text-lg border-x border-[#E5E5E5] py-2 px-4 hover:bg-black hover:text-white flex items-center relative"
+                >
+                    <h1 className="w-full">সাধারন প্রশ্ন উত্তর</h1>
+
+                    <FaCaretRight className="rotate-45 ml-1" />
+
+                    <ul
+                        className={`absolute top-10 left-0 outline-none bg-white border border-t-0 border-[#E5E5E5] w-full py-2 space-y-1 ${
+                            isCommon ? "visible" : "hidden"
+                        }`}
+                    >
+                        {links.map((link) => (
+                            <li key={link.path}>
+                                <Links link={link.title} path={link.path} />
+                            </li>
+                        ))}
+                    </ul>
                 </button>
 
-                <button className="bg-transparent text-black transition-all ease-in font-medium text-lg border-x border-[#E5E5E5] py-2 px-4 hover:bg-black hover:text-white">
+                <button
+                    onClick={() => setIsCommon2(!isCommon2)}
+                    className="bg-transparent text-black transition-all ease-in font-medium text-lg border-x border-[#E5E5E5] py-2 px-4 hover:bg-black hover:text-white flex items-center relative"
+                >
                     এজেন্ট লিষ্ট
+                    <FaCaretRight className="rotate-45 ml-1" />
+                    <ul
+                        className={`absolute top-10 left-0 outline-none bg-white border border-t-0 border-[#E5E5E5] w-full py-2 space-y-1 ${
+                            isCommon2 ? "visible" : "hidden"
+                        }`}
+                    >
+                        {agentLinks.map((link) => (
+                            <li key={link.path}>
+                                <Links link={link.title} path={link.path} />
+                            </li>
+                        ))}
+                    </ul>
                 </button>
             </div>
 
-            <hr className="bg-[#c00] h-[2px] -m-[2px] p-0" />
+            <hr className="bg-[#c00] h-[2px] -m-[2px] p-0 z-20 relative" />
         </div>
     );
 }
---
+++ src/Components/Shared/Navbar/Links.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+function Links({ link, path }) {
+    return (
+        <NavLink
+            to={path}
+            className={
+                "text-black bg-transparent hover:text-black hover:bg-gray-100 px-2 py-2"
+            }
+        >
+            <button className="w-[89%] text-xs">{link}</button>
+        </NavLink>
+    );
+}
+
+Links.propTypes = {
+    link: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired,
+};
+
+export default Links;
